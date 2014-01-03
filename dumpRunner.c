b5e9723c899fdcbee86613f6d59df930e3a38272
Max Grossman <max@delphix.com>
2014-01-03 15:00:58 -0800

    preliminary

--- dumpRunner.c
+++ dumpRunner.c
@@ -2,4 +2,18 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("usage: %s dump-file\n", argv[0]);
+        return 0;
+    }
+
+    FILE *in = fopen(argv[0], "r");
+
+    int nArgs;
+    fread(&nArgs, sizeof(int), 1, in);
+    printf("%d arguments", nArgs);
+
+    fclose(in);
+
+    return 0;
 }
